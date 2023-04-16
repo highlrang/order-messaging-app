05511b3cab2c6ef705eb8d38a599877e1c3af368
highlrang <jhw127@naver.com>
2023-04-16 21:33:37 +0900

    multi module generated

---
+++ store-api/src/test/java/com/myproject/storeapi/StoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.storeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
